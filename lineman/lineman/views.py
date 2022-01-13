97e89a0ffb9c16e901202a943e32010262226272
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2022-01-13 23:39:28 +0100

    fix network_mode in docker-compose

--- lineman/lineman/views.py
+++ lineman/lineman/views.py
@@ -26,4 +26,4 @@ def barrier_operations():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- train/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-celery --app=train.tasks.celery worker -Q station,speed --loglevel=info -E &&
-celery beat --app=train.tasks.celery --loglevel=info
